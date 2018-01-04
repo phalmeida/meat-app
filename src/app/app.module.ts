72ff1e8b68c500d8591f4299fad0d8749349c1f5
Philipe Allan Almeida <philipe.almeida@latitude.unb.br>
2018-01-04 19:43:11 -0200

    Criação do component order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import {ShoppingCartComponent} from './restaurants/restaurant-detail/shopping-ca
 import {MenuItemComponent} from './restaurants/restaurant-detail/menu-item/menu-item.component';
 import {ReviewsComponent} from './restaurants/restaurant-detail/reviews/reviews.component';
 import {ShoppingCartService} from "./restaurants/restaurant-detail/shopping-cart/shopping-cart.service";
+import {OrderComponent} from './order/order.component';
 
 
 @NgModule({
@@ -32,7 +33,8 @@ import {ShoppingCartService} from "./restaurants/restaurant-detail/shopping-cart
         MenuComponent,
         ShoppingCartComponent,
         MenuItemComponent,
-        ReviewsComponent
+        ReviewsComponent,
+        OrderComponent
     ],
     imports: [
         BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import {RestaurantsComponent} from "./restaurants/restaurants.component";
 import {RestaurantDetailComponent} from "./restaurants/restaurant-detail/restaurant-detail.component";
 import {MenuComponent} from "./restaurants/restaurant-detail/menu/menu.component";
 import {ReviewsComponent} from "./restaurants/restaurant-detail/reviews/reviews.component";
+import {OrderComponent} from "./order/order.component";
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
@@ -17,5 +18,6 @@ export const ROUTES: Routes = [
             {path: 'menu', component: MenuComponent},
             {path: 'reviews', component: ReviewsComponent}
         ]
-    }
+    },
+    {path: 'order', component: OrderComponent},
 ]
