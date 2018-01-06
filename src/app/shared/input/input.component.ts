9b47611443e88a68726700d2e3002fb0a42ccc2c
Philipe Allan Almeida <philipe.almeida@outlook.com>
2018-01-06 19:14:38 -0200

    Criação do componente de input.

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,14 +1,38 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input, ContentChild, AfterContentInit} from '@angular/core';
+import {NgModel} from "@angular/forms";
 
 @Component({
-  selector: 'mt-input',
-  templateUrl: './input.component.html'
+    selector: 'mt-input-container',
+    templateUrl: './input.component.html'
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, AfterContentInit {
 
-  constructor() { }
+    @Input() label: string;
+    @Input() errorMessage: string;
+    input: any;
 
-  ngOnInit() {
-  }
+    @ContentChild(NgModel) model: NgModel;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    ngAfterContentInit() {
+        this.input = this.model;
+        if (this.input === undefined) {
+            throw new Error('Esse Componente precisa ser usado com uma diretiva ngModel');
+        }
+
+    }
+
+    hasSuccess(): boolean {
+        return this.input.valid && (this.input.dirty || this.input.touched);
+    }
+
+    hasError(): boolean {
+        return this.input.invalid && (this.input.dirty || this.input.touched);
+    }
 
 }
